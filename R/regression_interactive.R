b583dc908a9d3ccda83cdc584f236976df16d8f3
daniele749 <106737685+daniele749@users.noreply.github.com>
2022-10-21 03:24:15 +0800

    Updated doc for interactive_regression()

--- R/regression_interactive.R
+++ R/regression_interactive.R
@@ -1,6 +1,6 @@
-#' interactive_regression() runs a regression simulation.
-#' Click on the plotting area to add points and see a corresponding regression line
-#'  (hitting ESC will stop the simulation). 
+#' compstatslib interactive_regression() function
+#' 
+#' Interactive visualization function that lets you point-and-click to add data points, while it automatically plots and updates a regression line and associated statistics.
 #'
 #' You can supply the following parameter:
 #'   old_pts - dataframe of x,y points to start interacting with
@@ -11,6 +11,35 @@
 #'   correlation - correlation of x and y
 #'   r-squared - R^2 of y
 #'   
+#' @param points A dataframe containing coordinates for estimating the regression. 
+#' @param ... Further arguments passed to or from other methods.
+#' 
+#' @return A dataframe containing the points coordinates. Additionally, the following parameters are provided on the plot area:
+#'  \item{Raw intercept}{The y-coordinate at which the regression line crosses the y-axis.}
+#'  \item{Raw slope}{The value of the slope parameter.}
+#'  \item{Correlation}{The strength of the linear relationship.}
+#'  \item{SSR}{The sum of squares regression.}
+#'  \item{SSE}{The sum of squares error.}
+#'  \item{SST}{The sum of squares total.}
+#'  \item{R-squared}{The multiple coefficient of determination.}
+#' 
+#' @usage 
+#' interactive_regression(points, ...)
+#' 
+#' Click on the plotting area to add points and see a corresponding regression line (hitting ESC will stop the simulation).
+#'
+#' @seealso \code{\link{plot_regr}}
+#' 
+#' @examples  
+#' 
+#' # Providing coordinates beforehand:
+#' points <- data.frame(x = c(1, 4, 7), y = c(2, 5, 8))
+#' 
+#' interactive_regression(points)
+#' 
+#' # Selecting coordinates on the plot area:
+#' interactive_regression()
+#'
 #' @export
 interactive_regression <- function(points=data.frame(), ...) {
   cat("Click on the plot to create data points; hit [esc] to stop")
