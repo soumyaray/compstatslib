163b74f231f0f5be2e904b56210bc5da57f991b8
Soumya Ray <soumya.ray@gmail.com>
2022-11-08 21:26:25 +0800

    Interactive sampling plots

--- R/sampling_interactive.R
+++ R/sampling_interactive.R
@@ -1,38 +1,64 @@
 #' @export
 interactive_sampling <- function(population, sample_size = 10, theta = mean) {
-  #   manipulate::manipulate(
-  #     plot_t_test(diff, sd, n, alpha),
-  #     diff  = manipulate::slider(0, 4, step = 0.1, initial = 0.5),
-  #     sd    = manipulate::slider(1, 5, step = 0.1, initial = 4),
-  #     n     = manipulate::slider(2, 500, step = 1, initial = 100),
-  #     alpha = manipulate::slider(0.01, 0.1, step = 0.01, initial = 0.05)
-  #   )
+  # plot_cache <- plot_sampling(population, sample_size, theta)
+  plot_cache <- NULL
 
-  plot_sampling(population, sample_size, theta)
-  # manipulate::manipulate()
+  manipulate::manipulate(
+    plot_cache <- plot_sampling(population, sample_size, theta, reps, vars = plot_cache),
+    sample_size = manipulate::picker(10, 100, 500, 1000, 5000, 10000),
+    reps = manipulate::picker(1, 5, 10, 50, 100),
+    run = manipulate::button("Sample")
+  )
 }
 
-plot_sampling <- function(population = NA, sample_size = 10,
-                          theta = mean, sample_means = c(), plot_vars = NA) {
-  xmin <- min(population)
-  xmax <- max(population)
+plot_sampling <- function(population, sample_size,
+                          theta, reps = 1, vars = NULL) {
+  if (!is.null(vars)) {
+    xmin <- vars$xmin
+    xmax <- vars$xmax
+    sample_means <- vars$sample_means
+  } else {
+    xmin <- min(population)
+    xmax <- max(population)
+    sample_means <- c()
+  }
 
   old_par <- par(mfrow = c(3, 1))
 
+  # population_sample <- sample(population, sample_size)
+  samples <- replicate(reps, sample(population, sample_size))
+  means <- apply(samples, FUN = theta, MARGIN = 2)
+  sample_means <- append(sample_means, means)
+
+  # TODO: skip population plot if plotting again?
   plot(density(population),
        xlim = c(xmin, xmax), xaxt = "n", frame = FALSE)
   axis(1)
 
-  population_sample <- sample(population, sample_size, )
-  plot(density(population_sample),
+
+  plot(density(samples),
                xlim = c(xmin, xmax), xaxt = "n", frame = FALSE)
   axis(1)
 
-  sample_means <- append(sample_means, theta(population_sample))
-  hist(sample_means, xlim = c(xmin, xmax), xaxt = "n")
+  if (length(sample_means) < 3) { breaks <- "Sturges" } else { breaks <- "FD" }
+
+  hist(sample_means, breaks = breaks, xlim = c(xmin, xmax), xaxt = "n")
   axis(1)
 
   par(old_par)
+
+  vars <- list(
+    population = population,
+    xmin = xmin,
+    xmax = xmax,
+    sample_size = sample_size,
+    theta = theta,
+    sample_means = sample_means
+  )
+
+  class(vars) <- append(class(vars), c("compstats", "sampling"))
+
+  vars
 }
 
 # population = rnorm(1000000)
