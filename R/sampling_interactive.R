55216ae63ef6f468464d733e1752efe6f77039f7
Soumya Ray <soumya.ray@gmail.com>
2022-11-11 21:31:53 +0800

    Plots all samples

--- R/sampling_interactive.R
+++ R/sampling_interactive.R
@@ -1,6 +1,7 @@
 #' @example
 #' # pop = rnorm(100000)
 #' # interactive_sampling(pop)
+#' # interactive_sampling(c(rnorm(100000, mean = 4), rnorm(100000, mean=-4)), theta = mean)
 #' @export
 interactive_sampling <- function(population, sample_size = 10, theta = mean) {
   plot_cache <- NULL
@@ -12,7 +13,9 @@ interactive_sampling <- function(population, sample_size = 10, theta = mean) {
     reps = manipulate::picker(1, 5, 10, 50, 100),
     run = manipulate::button("Sample")
   )
-  cat("finished")
+  cat("Use the gears icon (top-left of plot) to draw another set of samples.\n")
+  cat("Set the size of each of each sample (sample_size) drawn.\n")
+  cat("Choose how many repetitions (reps) of each sample should be drawn.\n")
 }
 
 plot_sampling <- function(population, sample_size, theta, reps = 1,
@@ -43,10 +46,14 @@ plot_sampling <- function(population, sample_size, theta, reps = 1,
 
   plot(samd, lty = "solid", lwd = 2, xlim = c(xmin, xmax), xaxt = "n",
        yaxt = "n", frame = FALSE, main = NA)
+  apply(samples, MARGIN=2, \(s) { 
+    lines(density(s), lty = "solid", col=rgb(0.7, 0.7, 0.7, 0.5), lwd = 1) })
+  lines(samd, lty = "solid", lwd = 2)
+  
   axis(1)
   text(xmin, max(samd$y)/2, boldly("Sample Distribution"), adj = 0)
 
-  # TODO: all user to fix bin size
+  # TODO: allow user to fix bin size
   # TODO: use some fraction of population hist height
   # if (length(sample_means) < 3) { breaks <- "Sturges" } else { breaks <- "FD" }
   samh <- hist(sample_means, border = FALSE,
