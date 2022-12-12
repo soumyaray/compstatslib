fb5902affaf693b77e397fd36d5e69abf492c68e
Daniele Melotti <danielemelotti@sd233200.ching-abc.ab.nthu.edu.tw>
2022-12-12 20:52:47 +0800

    Changed error matrix initial visibility state

--- R/t_statistic_interactive.R
+++ R/t_statistic_interactive.R
@@ -23,6 +23,6 @@ interactive_t_test <- function() {
     sd    = manipulate::slider(1, 5, step = 0.1, initial = 4),
     n     = manipulate::slider(2, 500, step = 1, initial = 100),
     alpha = manipulate::slider(0.01, 0.1, step = 0.01, initial = 0.05),
-    error_matrix = manipulate::checkbox(initial = TRUE, label = "Error Matrix")
+    error_matrix = manipulate::checkbox(initial = FALSE, label = "Error Matrix")
   )
 }
